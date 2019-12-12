2e6ee454a037a88e63c534e53341e37fa51cde19
agarcia117 <43732771+agarcia117@users.noreply.github.com>
2019-12-12 15:32:49 -0800

    Made RecyclerView a little better looking and clickable

--- app/src/main/java/com/example/locationnotification/CalendarFragment.kt
+++ app/src/main/java/com/example/locationnotification/CalendarFragment.kt
@@ -1,6 +1,10 @@
 package com.example.mobileprojectpractice
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.provider.ContactsContract.CommonDataKinds.Organization.TITLE
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +30,15 @@ class CalendarFragment : Fragment() {
         val calendar: CalendarView = binding.calendarViewMain
         val dateButton: Button = binding.buttonTest
         val textTest: TextView = binding.eventReminderHeader
-        binding.buttonTest.setOnClickListener { view : View -> view.findNavController().navigate(R.id.action_calendarFragment_to_eventCreatorFragment) }
+        binding.buttonTest.setOnClickListener { view : View -> view.findNavController().navigate(R.id.action_calendarFragment_to_eventCreatorFragment)
+//            val intent = Intent(Intent.ACTION_INSERT)
+//                .setData(CONTENT_URI)
+//                .putExtra(TITLE, "My Event")
+//                .putExtra(EVENT_LOCATION, "Here")
+//                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+//                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000))
+//            startActivity(intent)
+        }
         binding.viewEventsButton.setOnClickListener { view : View -> view.findNavController().navigate(R.id.action_calendarFragment_to_eventListFragment) }
 
 
